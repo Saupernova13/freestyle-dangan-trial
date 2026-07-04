54402b0dcaca687dfca7b73b659a949d862188d9
Sauraav Jayrajh <69242316+Saupernova13@users.noreply.github.com>
2026-07-05 00:08:40 +0200

    refactor(engine): drop Godot template boilerplate from frame_counter

--- freestyle-dangan-trial/scripts/tools/frame_counter.gd
+++ freestyle-dangan-trial/scripts/tools/frame_counter.gd
@@ -1,11 +1,5 @@
 extends Label
+## Debug FPS readout. Wired to a Label in the trial room scene.
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	text = "FPS: %d" % Engine.get_frames_per_second()
